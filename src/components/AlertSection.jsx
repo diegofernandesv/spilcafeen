d92a7f109402a40807531ed1d3a908891a9f95aa
Diego Fernandes <eaa24daf@students.eaaa.dk>
2025-03-08 22:19:54 +0100

    Second Commit

--- src/components/AlertSection.jsx
+++ src/components/AlertSection.jsx
@@ -6,12 +6,31 @@ function AlertSection() {
     <section className={styles.alerts}>
       <div className={styles.alertCard}>
         <div>
-          <div
-            dangerouslySetInnerHTML={{
-              __html:
-                "<svg id=&quot;5:4&quot; layer-name=&quot;ion:alert&quot; width=&quot;36&quot; height=&quot;36&quot; viewBox=&quot;0 0 36 36&quot; fill=&quot;none&quot; xmlns=&quot;http://www.w3.org/2000/svg&quot; class=&quot;alert-icon&quot; style=&quot;width: 36px; height: 36px&quot;> <path d=&quot;M18 5.625C17.3911 5.625 16.8342 6.1425 16.875 6.75L17.4375 21.9375C17.4375 22.0867 17.4968 22.2298 17.6023 22.3352C17.7077 22.4407 17.8508 22.5 18 22.5C18.1492 22.5 18.2923 22.4407 18.3978 22.3352C18.5032 22.2298 18.5625 22.0867 18.5625 21.9375L19.125 6.75C19.1658 6.1425 18.6089 5.625 18 5.625Z&quot; fill=&quot;black&quot; stroke=&quot;black&quot; stroke-linecap=&quot;round&quot; stroke-linejoin=&quot;round&quot;></path> <path d=&quot;M18 30.375C18.6213 30.375 19.125 29.8713 19.125 29.25C19.125 28.6287 18.6213 28.125 18 28.125C17.3787 28.125 16.875 28.6287 16.875 29.25C16.875 29.8713 17.3787 30.375 18 30.375Z&quot; stroke=&quot;black&quot; stroke-linecap=&quot;round&quot; stroke-linejoin=&quot;round&quot;></path> </svg>",
-            }}
-          />
+          <svg
+            id="5:4"
+            layer-name="ion:alert"
+            width="36"
+            height="36"
+            viewBox="0 0 36 36"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            className="alert-icon"
+            style={{ width: "36px", height: "36px" }}
+          >
+            <path
+              d="M18 5.625C17.3911 5.625 16.8342 6.1425 16.875 6.75L17.4375 21.9375C17.4375 22.0867 17.4968 22.2298 17.6023 22.3352C17.7077 22.4407 17.8508 22.5 18 22.5C18.1492 22.5 18.2923 22.4407 18.3978 22.3352C18.5032 22.2298 18.5625 22.0867 18.5625 21.9375L19.125 6.75C19.1658 6.1425 18.6089 5.625 18 5.625Z"
+              fill="black"
+              stroke="black"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+            <path
+              d="M18 30.375C18.6213 30.375 19.125 29.8713 19.125 29.25C19.125 28.6287 18.6213 28.125 18 28.125C17.3787 28.125 16.875 28.6287 16.875 29.25C16.875 29.8713 17.3787 30.375 18 30.375Z"
+              stroke="black"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
         </div>
         <div className={styles.alertText}>
           <span>3 new reviews need approval</span>
@@ -20,12 +39,22 @@ function AlertSection() {
       </div>
       <div className={styles.alertCard}>
         <div>
-          <div
-            dangerouslySetInnerHTML={{
-              __html:
-                "<svg id=&quot;5:20&quot; layer-name=&quot;ic:outline-emoji-events&quot; width=&quot;36&quot; height=&quot;36&quot; viewBox=&quot;0 0 36 36&quot; fill=&quot;none&quot; xmlns=&quot;http://www.w3.org/2000/svg&quot; class=&quot;alert-icon&quot; style=&quot;width: 36px; height: 36px&quot;> <path d=&quot;M28.5 7.5H25.5V4.5H10.5V7.5H7.5C5.85 7.5 4.5 8.85 4.5 10.5V12C4.5 15.825 7.38 18.945 11.085 19.41C11.5594 20.534 12.3003 21.5257 13.2437 22.2993C14.1872 23.0728 15.3048 23.6051 16.5 23.85V28.5H10.5V31.5H25.5V28.5H19.5V23.85C20.6952 23.6051 21.8128 23.0728 22.7563 22.2993C23.6997 21.5257 24.4406 20.534 24.915 19.41C28.62 18.945 31.5 15.825 31.5 12V10.5C31.5 8.85 30.15 7.5 28.5 7.5ZM7.5 12V10.5H10.5V16.23C8.76 15.6 7.5 13.95 7.5 12ZM18 21C15.525 21 13.5 18.975 13.5 16.5V7.5H22.5V16.5C22.5 18.975 20.475 21 18 21ZM28.5 12C28.5 13.95 27.24 15.6 25.5 16.23V10.5H28.5V12Z&quot; fill=&quot;black&quot;></path> </svg>",
-            }}
-          />
+          <svg
+            id="5:20"
+            layer-name="ic:outline-emoji-events"
+            width="36"
+            height="36"
+            viewBox="0 0 36 36"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            className="alert-icon"
+            style={{ width: "36px", height: "36px" }}
+          >
+            <path
+              d="M28.5 7.5H25.5V4.5H10.5V7.5H7.5C5.85 7.5 4.5 8.85 4.5 10.5V12C4.5 15.825 7.38 18.945 11.085 19.41C11.5594 20.534 12.3003 21.5257 13.2437 22.2993C14.1872 23.0728 15.3048 23.6051 16.5 23.85V28.5H10.5V31.5H25.5V28.5H19.5V23.85C20.6952 23.6051 21.8128 23.0728 22.7563 22.2993C23.6997 21.5257 24.4406 20.534 24.915 19.41C28.62 18.945 31.5 15.825 31.5 12V10.5C31.5 8.85 30.15 7.5 28.5 7.5ZM7.5 12V10.5H10.5V16.23C8.76 15.6 7.5 13.95 7.5 12ZM18 21C15.525 21 13.5 18.975 13.5 16.5V7.5H22.5V16.5C22.5 18.975 20.475 21 18 21ZM28.5 12C28.5 13.95 27.24 15.6 25.5 16.23V10.5H28.5V12Z"
+              fill="black"
+            />
+          </svg>
         </div>
         <div className={styles.alertText}>
           <span>Monopoly Tournament is in:</span>
--- src/components/RatingsChart.jsx
+++ src/components/RatingsChart.jsx
@@ -7,33 +7,111 @@ function RatingsChart() {
       <h3 className={styles.sectionTitle}>Top rated games by ratings:</h3>
       <div className={styles.ratingsChart}>
         <div className={styles.chartBarsecond}>
+          <div className={styles.gameName}>Game B</div> {/* Add game name */}
           <div>
-            <div
-              dangerouslySetInnerHTML={{
-                __html:
-                  "<svg id=&quot;5:94&quot; width=&quot;89&quot; height=&quot;195&quot; viewBox=&quot;0 0 89 195&quot; fill=&quot;none&quot; xmlns=&quot;http://www.w3.org/2000/svg&quot; xmlns:xlink=&quot;http://www.w3.org/1999/xlink&quot; class=&quot;game-icon&quot; style=&quot;width: 89px&quot;> <circle cx=&quot;44.5&quot; cy=&quot;25.5&quot; r=&quot;25.5&quot; fill=&quot;url(#pattern0_5_94)&quot;></circle> <path d=&quot;M0 71C0 66.5817 3.58172 63 8 63H81C85.4183 63 89 66.5817 89 71V195H0V71Z&quot; fill=&quot;#ED1433&quot;></path> <text fill=&quot;white&quot; xml:space=&quot;preserve&quot; style=&quot;white-space: pre&quot; font-family=&quot;Aeonik TRIAL&quot; font-size=&quot;24&quot; letter-spacing=&quot;0em&quot;><tspan x=&quot;24&quot; y=&quot;177.14&quot;>2nd</tspan></text> <defs> <pattern id=&quot;pattern0_5_94&quot; patternContentUnits=&quot;objectBoundingBox&quot; width=&quot;1&quot; height=&quot;1&quot;> <use xlink:href=&quot;#image0_5_94&quot; transform=&quot;translate(-0.207386) scale(0.00142045)&quot;></use> </pattern>  </defs> </svg>",
-              }}
-            />
+            <svg
+              id="5:94"
+              width="89"
+              height="195"
+              viewBox="0 0 89 195"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              className="game-icon"
+              style={{ width: "89px" }}
+            >
+              <circle cx="44.5" cy="25.5" r="25.5" fill="url(#pattern0_5_94)"></circle>
+              <path
+                d="M0 71C0 66.5817 3.58172 63 8 63H81C85.4183 63 89 66.5817 89 71V195H0V71Z"
+                fill="#ED1433"
+              ></path>
+              <text
+                fill="white"
+                xmlSpace="preserve"
+                style={{ whiteSpace: "pre" }}
+                fontFamily="Aeonik TRIAL"
+                fontSize="24"
+                letterSpacing="0em"
+              >
+                <tspan x="24" y="177.14">2nd</tspan>
+              </text>
+              <defs>
+                <pattern id="pattern0_5_94" patternContentUnits="objectBoundingBox" width="1" height="1">
+                  <use xlinkHref="#image0_5_94" transform="translate(-0.207386) scale(0.00142045)"></use>
+                </pattern>
+              </defs>
+            </svg>
           </div>
         </div>
         <div className={styles.chartBarfirst}>
+          <div className={styles.gameName}>Game A</div> {/* Add game name */}
           <div>
-            <div
-              dangerouslySetInnerHTML={{
-                __html:
-                  "<svg id=&quot;5:93&quot; width=&quot;89&quot; height=&quot;255&quot; viewBox=&quot;0 0 89 255&quot; fill=&quot;none&quot; xmlns=&quot;http://www.w3.org/2000/svg&quot; xmlns:xlink=&quot;http://www.w3.org/1999/xlink&quot; class=&quot;game-icon&quot; style=&quot;width: 89px&quot;> <circle cx=&quot;44.5&quot; cy=&quot;25.5&quot; r=&quot;25.5&quot; fill=&quot;url(#pattern0_5_93)&quot;></circle> <path d=&quot;M0 68C0 63.5817 3.58172 60 8 60H81C85.4183 60 89 63.5817 89 68V255H0V68Z&quot; fill=&quot;#ED1433&quot;></path> <text fill=&quot;white&quot; xml:space=&quot;preserve&quot; style=&quot;white-space: pre&quot; font-family=&quot;Aeonik TRIAL&quot; font-size=&quot;24&quot; letter-spacing=&quot;0em&quot;><tspan x=&quot;30.5&quot; y=&quot;237.14&quot;>1st </tspan></text> <defs> <pattern id=&quot;pattern0_5_93&quot; patternContentUnits=&quot;objectBoundingBox&quot; width=&quot;1&quot; height=&quot;1&quot;> <use xlink:href=&quot;#image0_5_93&quot; transform=&quot;translate(-0.207386) scale(0.00142045)&quot;></use> </pattern>  </defs> </svg>",
-              }}
-            />
+            <svg
+              id="5:93"
+              width="89"
+              height="255"
+              viewBox="0 0 89 255"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              className="game-icon"
+              style={{ width: "89px" }}
+            >
+              <circle cx="44.5" cy="25.5" r="25.5" fill="url(#pattern0_5_93)"></circle>
+              <path
+                d="M0 68C0 63.5817 3.58172 60 8 60H81C85.4183 60 89 63.5817 89 68V255H0V68Z"
+                fill="#ED1433"
+              ></path>
+              <text
+                fill="white"
+                xmlSpace="preserve"
+                style={{ whiteSpace: "pre" }}
+                fontFamily="Aeonik TRIAL"
+                fontSize="24"
+                letterSpacing="0em"
+              >
+                <tspan x="30.5" y="237.14">1st </tspan>
+              </text>
+              <defs>
+                <pattern id="pattern0_5_93" patternContentUnits="objectBoundingBox" width="1" height="1">
+                  <use xlinkHref="#image0_5_93" transform="translate(-0.207386) scale(0.00142045)"></use>
+                </pattern>
+              </defs>
+            </svg>
           </div>
         </div>
         <div className={styles.chartBarthird}>
+          <div className={styles.gameName}>Game C</div> {/* Add game name */}
           <div>
-            <div
-              dangerouslySetInnerHTML={{
-                __html:
-                  "<svg id=&quot;5:92&quot; width=&quot;89&quot; height=&quot;120&quot; viewBox=&quot;0 0 89 120&quot; fill=&quot;none&quot; xmlns=&quot;http://www.w3.org/2000/svg&quot; xmlns:xlink=&quot;http://www.w3.org/1999/xlink&quot; class=&quot;game-icon&quot; style=&quot;width: 89px&quot;> <circle cx=&quot;44.5&quot; cy=&quot;25.5&quot; r=&quot;25.5&quot; fill=&quot;url(#pattern0_5_92)&quot;></circle> <path d=&quot;M0 64C0 59.5817 3.58172 56 8 56H81C85.4183 56 89 59.5817 89 64V120H0V64Z&quot; fill=&quot;#ED1433&quot;></path> <text fill=&quot;white&quot; xml:space=&quot;preserve&quot; style=&quot;white-space: pre&quot; font-family=&quot;Aeonik TRIAL&quot; font-size=&quot;24&quot; letter-spacing=&quot;0em&quot;><tspan x=&quot;26.5&quot; y=&quot;102.14&quot;>3rd</tspan></text> <defs> <pattern id=&quot;pattern0_5_92&quot; patternContentUnits=&quot;objectBoundingBox&quot; width=&quot;1&quot; height=&quot;1&quot;> <use xlink:href=&quot;#image0_5_92&quot; transform=&quot;translate(-0.207386) scale(0.00142045)&quot;></use> </pattern>  </defs> </svg>",
-              }}
-            />
+            <svg
+              id="5:92"
+              width="89"
+              height="120"
+              viewBox="0 0 89 120"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              className="game-icon"
+              style={{ width: "89px" }}
+            >
+              <circle cx="44.5" cy="25.5" r="25.5" fill="url(#pattern0_5_92)"></circle>
+              <path
+                d="M0 64C0 59.5817 3.58172 56 8 56H81C85.4183 56 89 59.5817 89 64V120H0V64Z"
+                fill="#ED1433"
+              ></path>
+              <text
+                fill="white"
+                xmlSpace="preserve"
+                style={{ whiteSpace: "pre" }}
+                fontFamily="Aeonik TRIAL"
+                fontSize="24"
+                letterSpacing="0em"
+              >
+                <tspan x="26.5" y="102.14">3rd</tspan>
+              </text>
+              <defs>
+                <pattern id="pattern0_5_92" patternContentUnits="objectBoundingBox" width="1" height="1">
+                  <use xlinkHref="#image0_5_92" transform="translate(-0.207386) scale(0.00142045)"></use>
+                </pattern>
+              </defs>
+            </svg>
           </div>
         </div>
       </div>
