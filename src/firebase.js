eef1624d515966beb0880199558a12ee1ba26659
Diego Fernandes <eaa24daf@students.eaaa.dk>
2025-03-09 02:15:09 +0100

    Login version aeonix

--- src/firebase.js
+++ src/firebase.js
@@ -3,19 +3,18 @@ import { getAuth, GoogleAuthProvider, FacebookAuthProvider } from "firebase/auth
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCm-Fo2aoVy3G8DbSBmTSTP4EdS_BVd-o0",
-    authDomain: "spilcafeen-e8a10.firebaseapp.com",
-    projectId: "spilcafeen-e8a10",
-    storageBucket: "spilcafeen-e8a10.firebasestorage.app",
-    messagingSenderId: "969112916966",
-    appId: "1:969112916966:web:ffd4e2adcb0986fa50fc27",
-    measurementId: "G-2TKLL1J383"
+  apiKey: "AIzaSyCm-Fo2aoVy3G8DbSBmTSTP4EdS_BVd-o0",
+  authDomain: "spilcafeen-e8a10.firebaseapp.com",
+  projectId: "spilcafeen-e8a10",
+  storageBucket: "spilcafeen-e8a10.firebasestorage.app",
+  messagingSenderId: "969112916966",
+  appId: "1:969112916966:web:ffd4e2adcb0986fa50fc27",
+  measurementId: "G-2TKLL1J383"
 };
 
-const appFirebase = initializeApp(firebaseConfig);
-
-const auth = getAuth(appFirebase);
-const db = getFirestore(appFirebase);
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
 const providerGoogle = new GoogleAuthProvider();
 const providerFacebook = new FacebookAuthProvider();
 const authProviders = auth;
